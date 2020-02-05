718ea89f022ad9dd48b74065324139ecef8b5f1b
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-02-05 10:56:39 +0700

    Add starting code

--- workshop4/class4_kinship.pl
+++ workshop4/class4_kinship.pl
@@ -1,1 +1,15 @@
-
+parent(pam,bob).
+parent(tom,bob).
+parent(tom,liz).
+parent(bob,ann).
+parent(bob,pat).
+parent(pat,jim).
+father(X,Y) :- parent(X,Y), male(X).
+mother(X,Y) :- parent(X,Y),female(X).
+grandparent(X,Z) :- parent(X,Y), parent(Y,Z).
+child(X,Y) :- parent(Y,X).
+sister(X,Y) :- parent(Z,X), parent(Z,Y), female(X), X\=Y.
+ancester(X,Y) :- parent(X,Y).
+ancester(X,Y) :- parent(X,W), ancester(W,Y).
+same(X,Y) :- X=Y.
+diff(X,Y) :- not(same(X,Y)).
