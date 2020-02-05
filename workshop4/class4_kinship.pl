be260c4db18b54740b2d983779f9ec85c60ab07e
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-02-05 12:16:31 +0700

    Fix relationship script

--- workshop4/class4_kinship.pl
+++ workshop4/class4_kinship.pl
@@ -30,6 +30,7 @@ person(rose_granger-weasley).
 person(roxanne_weasley).
 person(vernon_dursley).
 person(victoire_weasley).
+ 
 
 % Properties
 male(albus_potter).
@@ -49,7 +50,6 @@ female(ginny_weasley).
 male(harry_potter).
 female(hermione_granger).
 male(hugo_granger-weasley).
-
 male(james_potter).
 male(james_s_potter).
 female(lilly_potter).
@@ -64,3 +64,81 @@ female(rose_granger-weasley).
 female(roxanne_weasley).
 male(vernon_dursley).
 female(victoire_weasley).
+
+
+% Relationships
+parent(albus_potter, ginny_weasley).
+parent(albus_potter, harry_potter).
+spouse(angelina_johnson, george_weasley).
+spouse(arthur_weasley, molly_weasley).
+parent(bill_weasley, arthur_weasley).
+parent(bill_weasley, molly_weasley).
+spouse(bill_weasley, fleur_delacour).
+parent(charlie_weasley, arthur_weasley).
+parent(charlie_weasley, molly_weasley).
+parent(dominique_weasley, bill_weasley).
+parent(dominique_weasley, fleur_delacour).
+parent(dudley_dursley, petunia_dursley).
+parent(dudley_dursley, vernon_dursley).
+spouse(euphemia_potter, fleamont_potter).
+spouse(fleamont_potter, euphemia_potter).
+spouse(fleur_delacour, bill_weasley).
+parent(fred_johnson_weasley, angelina_johnson).
+parent(fred_johnson_weasley, george_weasley).
+parent(fred_weasley, arthur_weasley).
+parent(fred_weasley, molly_weasley).
+parent(george_weasley, arthur_weasley).
+parent(george_weasley, molly_weasley).
+spouse(george_weasley, angelina_Johnson).
+parent(ginny_weasley, arthur_weasley).
+parent(ginny_weasley, molly_weasley).
+spouse(ginny_weasley, harry_potter).
+parent(harry_potter, lilly_potter).
+parent(harry_potter, james_potter).
+spouse(harry_potter, ginny_weasley).
+spouse(hermione_granger, ron_weasley).
+parent(hugo_granger-weasley, ron_weasley).
+parent(hugo_granger-weasley, hermione_granger).
+parent(james_potter, euphemia_potter).
+parent(james_potter, fleamont_potter).
+spouse(james_potter, lilly_potter).
+parent(james_s_potter, harry_potter).
+parent(james_s_potter, ginny_weasley).
+spouse(lilly_potter, james_potter).
+parent(lilly_l_potter, harry_potter).
+parent(lilly_l_potter, ginny_weasley).
+parent(louis_weasley, bill_weasley).
+parent(louis_weasley, fleur_delacour).
+spouse(molly_weasley, arthur_weasley).
+parent(percy_weasley, arthur_weasley).
+parent(percy_weasley, molly_weasley).
+spouse(petunia_dursley, vernon_dursley).
+parent(ron_weasley, arthur_weasley).
+parent(ron_weasley, molly_weasley).
+spouse(ron_weasley, hermione_granger).
+parent(rose_granger-weasley, ron_weasley).
+parent(rose_granger-weasley, hermione_granger).
+parent(roxanne_weasley, angelina_johnson).
+parent(roxanne_weasley, george_weasley).
+spouse(vernon_dursley, petunia_dursley).
+parent(victoire_weasley, bill_weasley).
+parent(victoire_weasley, fleur_delacour).
+
+
+% Rules
+mother(X,Y) :- female(X), parent(X,Y). %✅
+father(X,Y) :- male(X), parent(X,Y).
+child(X,Y) :- parent(Y,X).
+son(X,Y) :- child(X,Y), male(X).
+daughter(X,Y) :- child(X,Y), female(X).
+sibling(X,Y) :- parent(Z,X), parent(Z,Y), X \= Y.
+sister(X,Y) :- sibling(X,Y), female(X).
+brother(X,Y) :- sibling(X,Y), male(X).
+husband(X,Y) :- spouse(X,Y), male(X).
+wife(X,Y) :- spouse(X,Y), female(X).
+
+cousin(X,Y) :- child(X,sibling(Z,parent(W,Y))).
+niece(X,Y) :- daughter(X,Z), sibling(Z,Y).
+niece(X,Y) :- daughter(X,Z), sibling(Z,W), spouse(W,Y).
+grandmother(X,Y) :- mother(X,Z), parent(Z,Y).
+grandfather(X,Y) :- father(X,Z), parent(Z,Y).
--- workshop4/relationship.py
+++ workshop4/relationship.py
@@ -4,17 +4,17 @@ from itertools import islice
 def print_code(parts: list):
     # person,parent,parent,spouse
     if parts[1]:
-        print(f'parent({parts[0]}, {parts[1]})')
+        print(f'parent({parts[1]}, {parts[0]}).')
     if parts[2]:
-        print(f'parent({parts[0]}, {parts[2]})')
+        print(f'parent({parts[2]}, {parts[0]}).')
     if parts[3]:
-        print(f'spouse({parts[0]}, {parts[3]})')
+        print(f'spouse({parts[0]}, {parts[3]}).')
 
 
 if __name__ == '__main__':
-    file = open('workshop4/relationship.csv')
+    file = open('relationship.csv')
     text = file.read()
     lines = text.splitlines()
-    for line in islice(lines, 2, None):
+    for line in islice(lines, 1, None):
         parts = line.split(',')
         print_code(parts)
