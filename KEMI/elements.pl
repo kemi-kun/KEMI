9780eae3e05ee3bd2692727589d02c93c2333c74
T. Bunratta <tharathorn.bu@ku.th>
2020-02-20 18:19:35 +0700

    More cleanup

--- KEMI/elements.pl
+++ KEMI/elements.pl
@@ -2,8 +2,7 @@
     Facts
 
 */
-
-:- use_module('str', [replace/4, capitalize/2]).
+:- module(elements,[metal/1,nonmetal/1, extract_elements_from_formula/2]).
 
 
 % Elements
@@ -23,32 +22,6 @@ element_name("Cl", chlorine).
 
 metal("Na").
 nonmetal("Cl").
-
-basic_metal_cation(Symbol, Name) :-
-    metal(Symbol),
-    element_name(Symbol, Name).
-
-basic_nonmetal_anion(Symbol, Name) :-
-    nonmetal(Symbol),
-    element_name(Symbol, ElementName),
-    replace(ElementName, "ine", "", NextPrefix),
-    string_concat(NextPrefix, "ide", Name).
-
-%!  symbol_to_name_ionic(+Formula: string, -Name: string) 
-%
-%   Given a molecular formula,
-%   then resolve its name.
-%
-%   TODO: more complications
-symbol_to_name_ionic(Formula, Name) :-
-    extract_elements(Formula, [H, T|_]),
-    atom_string(H, Next),
-    basic_metal_cation(Next, Prefix),
-    capitalize(Prefix, Caps),
-    atom_string(T, Anion),
-    basic_nonmetal_anion(Anion, Suffix),
-    string_concat(Caps, " ", Then),
-    string_concat(Then, Suffix, Name).
 
 %
 %
@@ -81,28 +54,4 @@ extract_elements_(Formula, Start, String, End) :-
 
 
 
-% % compound(methane, chemical_element(hydrogen), chemical_element(carbon)).
 
-% extract_elements(String, Elements) :-
-%     write_ln("base"),
-%     string_length(String, Length),
-%     Length = 1,
-%     Elements = [String],
-%     !.
-
-% extract_elements(String, Elements) :-
-%     length(Elements, ListLength),
-%     write_ln("2"),
-%     write_ln(String),
-%     string_length(String, Length),
-%     SubLength is Length-1,
-%     Length > 1,
-%     sub_string(String, 0, SubLength, 1, OutString),
-%     write_ln(OutString),
-    
-%     extract_elements(OutString, ElementsB),
-%     append(ElementsB, [String], Elements),
-%    % NaCl
-    
-%     % append(Output, [], Output2),
-%     true.
