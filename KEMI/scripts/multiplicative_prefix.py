48480285eb1b42f76329374ee16e416d031e1814
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-02-20 18:42:02 +0700

    Add multiplicative prefix facts & script

---
+++ KEMI/scripts/multiplicative_prefix.py
@@ -0,0 +1,22 @@
+def load_data() -> dict:    
+    data = {}
+    with open('../assets/multiplicative-prefixes.csv') as f:
+        lines = f.read().splitlines()
+        for line in lines:
+            number, prefix, ligand_prefix = line.split(',')
+            data[number] = [prefix, ligand_prefix]
+    return data
+
+def print_multiplicative_prefixes(data: dict):
+    for number, val in data.items():
+        print(f'multiplicative_prefix({number}, "{val[0]}").')
+
+def print_multiplicative_ligand_prefixes(data: dict):
+    for number, val in data.items():
+        if val[1]:
+            print(f'multiplicative_ligand_prefix({number}, "{val[1]}").')
+
+if __name__ == "__main__":
+    data = load_data()
+    print_multiplicative_prefixes(data)
+    print_multiplicative_ligand_prefixes(data)
