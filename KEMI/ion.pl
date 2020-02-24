7f7f80394eb8729a2a2e071db7ded24e597b02a9
T. Bunratta <tharathorn.bu@ku.th>
2020-02-24 14:13:07 +0700

    Refactor ion functions

--- KEMI/ion.pl
+++ KEMI/ion.pl
@@ -23,30 +23,76 @@ element_prefix_(Symbol, Prefix) :-
     element_name(Symbol, Name),
     replace_ending_ma_(Name, Prefix).
 
+%
+% IR-5.3.2.2
+%
+monatomic_cation_name(ElementName, Result) :-
+    element_name(_, ElementName),
+    Result = ElementName.
+
+%
+% IR-5.3.2.3
+%
+homopolyatomic_cation_name(Term, Result) :-
+    extract_term(Term, [Symbol, Quantity|_]),
+    Quantity > 1,
+    element_name(Symbol, E),
+    multiplicative_prefix(Quantity, Prefix),
+    string_concat(Prefix, E, Result).
+
+%
+% IR-5.3.2.4
+%
+heteropolyatomic_cation_name([First, Second|_], Name) :-
+    % Charge < 0,
+    % Quantified is -Charge,
+    extract_term(First, [FSymbol, FQ|_]),
+    extract_term(Second, [SSymbol, SQ|_]),
+    engt(FSymbol, SSymbol, Terminal),
+    (FSymbol == Terminal, Q = FQ; Q = SQ),
+    delete([FSymbol, SSymbol], Terminal, [Central|_]),
+    element_prefix_(Terminal, Prefix),
+    element_name(Central, Suffix),
+    (Q > 1, multiplicative_prefix(Q, Mult),
+    string_concat(Mult, Prefix, S1), !; S1 = Prefix),
+    string_concat(S1, "ido", S2),
+    string_concat(S2, Suffix, Name).
+
+%
+% IR-5.3.3.2
+%
 monatomic_anion_name(ElementName, Result) :-
     element_name(Sym, ElementName),
     element_prefix_(Sym, Name),
     string_concat(Name, "ide", Result).
 
+%
+% IR-5.3.3.3
+%
 homopolyatomic_anion_name(Term, Result) :-
     extract_term(Term, [Symbol, Quantity|_]),
     Quantity > 1,
-    element_name(Symbol, _),
+    element_prefix_(Symbol, E),
     multiplicative_prefix(Quantity, Prefix),
-    element_prefix_(Symbol, ElementPrefix),
-    string_concat(Prefix, ElementPrefix, Name),
+    string_concat(Prefix, E, Name),
     string_concat(Name, "ide", Result).
 
-basic_cation(Term, Name) :-
-    extract_term(Term, [Symbol, Quantity|_]),
-    Quantity > 1,
-    element_name(Symbol, ElementName),
-    multiplicative_prefix(Quantity, Prefix),
-    string_concat(Prefix, ElementName, Name).
-
-basic_cation(Term, Name) :-
-    extract_term(Term, [Symbol|_]),
-    element_name(Symbol, Name).
+%
+% IR-5.3.3.4
+%
+heteropolyatomic_anion_name([First, Second|_], Name) :-
+    extract_term(First, [FSymbol, FQ|_]),
+    extract_term(Second, [SSymbol, SQ|_]),
+    engt(FSymbol, SSymbol, Terminal),
+    (FSymbol == Terminal, Q = FQ; Q = SQ),
+    delete([FSymbol, SSymbol], Terminal, [Central|_]),
+    element_prefix_(Terminal, Prefix),
+    element_prefix_(Central, Suffix),
+    (Q > 1, multiplicative_prefix(Q, Mult),
+     string_concat(Mult, Prefix, S1); S1 = Prefix),
+    string_concat(S1, "ido", S2),
+    string_concat(S2, Suffix, S3),
+    string_concat(S3, "ate", Name).
 
 %!  binary_stoichiometric_name(+Formula: string, -Name: string) 
 %
@@ -56,7 +102,7 @@ basic_cation(Term, Name) :-
 %   TODO: more sophistication
 binary_stoichiometric_name(Formula, Name) :-
     extract_elements_from_formula(Formula, [Cation|Anion]),
-    basic_cation(Cation, Prefix),
+    classify_cation(Cation, Prefix),
     Caps = Prefix,
     classify_anion(Anion, Suffix),
     string_concat(Caps, " ", Then),
@@ -81,6 +127,25 @@ engt(Element1, Element2, Result) :-
     Result = Element2,
     !.
 
+classify_cation(Any, Cation) :-
+    % length(Any, 1),
+    % [H|_] = Any,
+    extract_term(Any, [Symbol, Quantity|_]),
+    Quantity = 1,
+    element_name(Symbol, ElementName),
+    monatomic_cation_name(ElementName, Cation),
+    !.
+
+classify_cation(Any, Cation) :-
+    length(Any, 1),
+    [H|_] = Any,
+    homopolyatomic_cation_name(H, Cation),
+    !.
+
+classify_cation(Any, Cation) :-
+    heteropolyatomic_cation_name(Any, Cation),
+    !.
+
 classify_anion(Any, Anion) :-
     length(Any, 1),
     [H|_] = Any,
@@ -99,29 +164,6 @@ classify_anion(Any, Anion) :-
 classify_anion(Any, Anion) :-
     heteropolyatomic_anion_name(Any, Anion),
     !.
-
-%
-% IR-5.3.3.4
-%
-heteropolyatomic_anion_name([First, Second|_], Name) :-
-    % Charge < 0,
-    % Quantified is -Charge,
-    extract_term(First, [FSymbol, FQ|_]),
-    extract_term(Second, [SSymbol, SQ|_]),
-    engt(FSymbol, SSymbol, Terminal),
-    (FSymbol == Terminal, Q = FQ; Q = SQ),
-    delete([FSymbol, SSymbol], Terminal, [Central|_]),
-    element_prefix_(Terminal, Prefix),
-    element_prefix_(Central, Suffix),
-    (Q > 1, multiplicative_prefix(Q, Mult),
-     string_concat(Mult, Prefix, S1); S1 = Prefix),
-    string_concat(S1, "ido", S2),
-    string_concat(S2, Suffix, S3),
-    string_concat(S3, "ate", Name).
-    % string_concat(StepOne, "(", StepTwo),
-    % string_concat(StepTwo, Quantified, StepThree),
-    % string_concat(StepThree, "-)", Name).
-    % extract_term(Term, [Symbol, Quantity|_]).
 
 % heteropolyatomic_anion([First, Second|_], Name) :-
 %     engt(First, Second, R),
