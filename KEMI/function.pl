07a57b21f3ef0978e9b11ea01ab0528ecfa9e11f
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-03-25 21:59:28 +0700

    Adjust spaces in documentation

--- KEMI/function.pl
+++ KEMI/function.pl
@@ -1,20 +1,19 @@
-%! list_remove(+In: list, +Element: atom, -Out: list) is det.
-%! list_remove(-In: list, +Element: atom, +Out: list) is det.
+%!  list_remove(+In: list, +Element: atom, -Out: list) is det.
+%!  list_remove(-In: list, +Element: atom, +Out: list) is det.
 %  
-%  Remove the first appearance of `Element` in list `In`
-%  and return the result list as `Out`.
-%  Return false if `Element` is not in `In`
+%   Remove the first appearance of `Element` in list `In`
+%   and return the result list as `Out`.
+%   Return false if `Element` is not in `In`
 %  
 list_remove(In, Element, Out) :-
     select(Element, In, Out), 
     !.
 
-%! reversed(+In: list, -Out: list) is det.
+%!  reversed(+In: list, -Out: list) is det.
 %  
-%  Reverse list `In`and return the result list as `Out`.
-%  Return false if `In` is not list
+%   Reverse list `In`and return the result list as `Out`.
+%   Return false if `In` is not list
 %
 reversed(In, Out) :-
     reverse(In, Out),
     !.
-
