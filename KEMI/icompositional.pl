4598f0bdb5fbc2287a17cd49357b3f83b3f52dc3
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-03-28 19:23:55 +0700

    Re-add HomonuclearCN()

--- KEMI/icompositional.pl
+++ KEMI/icompositional.pl
@@ -1,8 +1,18 @@
 :- module(icompositional,[boron_hydride_stoichiometric/2,general_stoichiometric/2,addition_compound_cn/2,ion_cn/2,binary_compound_cn/2,homonuclear_cn/2]).
+:- use_module(chemutils,[get_element/3]).
+:- use_module(elements,[element_name/2]).
 
 
 homonuclear_cn(Formula, Name) :-
-    fail.
+    get_element(Formula, 0, Element),
+    get_num_atoms(Formula, Element, Amount),
+    element_name(Element, ElementName),
+    group(Element, 18) ->
+        mul_prefix_except_mono(Amount, MulPrefix);
+    multiplicative_prefix(Amount, MulPrefix),
+    string_concat(MulPrfix, ElementName, Name).
+ 
+ 
 binary_compound_cn(Formula, Name) :-
     fail.
 ion_cn(Formula, Name) :-
