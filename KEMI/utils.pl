fda3dd6ed1df2e565349575f14c8e3fd5635552a
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-03-28 18:10:45 +0700

    Edit documentation: split_decimal()

--- KEMI/utils.pl
+++ KEMI/utils.pl
@@ -1,12 +1,17 @@
 :- module(utils,[split_decimal/2]).
 
 
-%! split_decimal(+Number: integer, -Numbers: list) is det.
-%! split_decimal(-Number: integer, +Numbers: list) is det.
-% 
-%  Return the number splitted into digits.
-%  
-%  split_decimal(1234, [1, 2, 3, 4]).
+%!  split_decimal(+Number:integer, +Numbers:list) is det.
+%!  split_decimal(+Number:integer, -Numbers:list) is det.
+%!  split_decimal(-Number:integer, -Numbers:list) is failure.
+%!  split_decimal(-Number:integer, +Numbers:list) is det.
+%
+%   Return the number splitted into digits.
+%
+%   Example:
+%       ?- split_decimal(1234, [1, 2, 3, 4]).
+%       true.
+%
 split_decimal(Number, Numbers) :-
     nonvar(Number),
     number_chars(Number, Numbers_),
