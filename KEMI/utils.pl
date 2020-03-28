0745b85e2769a1ee731be56f4b00e8a487af3476
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-03-28 14:28:45 +0700

    Added backtracking capabilities to `split_decimal()`

--- KEMI/utils.pl
+++ KEMI/utils.pl
@@ -2,10 +2,18 @@
 
 
 %! split_decimal(+Number: integer, -Numbers: list) is det.
+%! split_decimal(-Number: integer, +Numbers: list) is det.
 % 
 %  Return the number splitted into digits.
 %  
 %  split_decimal(1234, [1, 2, 3, 4]).
 split_decimal(Number, Numbers) :-
+    var(Numbers),
     number_chars(Number, Numbers_),
-    maplist(atom_number, Numbers_, Numbers).
+    maplist(atom_number, Numbers_, Numbers),
+    !.
+split_decimal(Number, Numbers) :-
+    var(Number),
+    maplist(atom_number, Numbers_, Numbers),
+    number_chars(Number, Numbers_),
+    !.
