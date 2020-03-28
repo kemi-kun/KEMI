f69f50d547e34d485d2886297a70fbd95273c0b2
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-03-28 21:21:05 +0700

    Refactor homonuclear_cn()

--- KEMI/icompositional.pl
+++ KEMI/icompositional.pl
@@ -1,18 +1,26 @@
 :- module(icompositional,[boron_hydride_stoichiometric/2,general_stoichiometric/2,addition_compound_cn/2,ion_cn/2,binary_compound_cn/2,homonuclear_cn/2]).
-:- use_module(chemutils,[get_element/3]).
-:- use_module(elements,[element_name/2]).
+:- use_module(chemutils,[get_element/3,get_all_elements/2,get_num_atoms/3]).
+:- use_module(elements,[element_name/2,group/2]).
+:- use_module(support,[multiplicative_prefix/2,mul_prefix_except_mono/2]).
 
 
 homonuclear_cn(Formula, Name) :-
-    get_element(Formula, 0, Element),
+    homonuclear(Formula),
+    get_all_elements(Formula, Elements),
+    memberchk(Element, Elements),
     get_num_atoms(Formula, Element, Amount),
     element_name(Element, ElementName),
-    group(Element, 18) ->
-        mul_prefix_except_mono(Amount, MulPrefix);
-    multiplicative_prefix(Amount, MulPrefix),
-    string_concat(MulPrfix, ElementName, Name).
+    (
+        group(Element, 18) ->
+            mul_prefix_except_mono(Amount, MulPrefix);
+        multiplicative_prefix(Amount, MulPrefix)
+    ),
+    string_concat(MulPrefix, ElementName, Name).
  
- 
+homonuclear(Formula) :-
+    get_all_elements(Formula, Elements),
+    length(Elements,1).
+
 binary_compound_cn(Formula, Name) :-
     fail.
 ion_cn(Formula, Name) :-
