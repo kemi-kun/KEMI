ffa5c39b4009335a65cc1032d009872751d83835
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-03-29 11:01:34 +0700

    Add `split_decimal()`

--- KEMI/utils.pl
+++ KEMI/utils.pl
@@ -1,7 +1,7 @@
-:- module(utils,[split_digits/2]).
+:- module(utils,[split_decimal/2]).
 
 
-%!  split_digits(+Number:int, +Numbers:list) is det.
+%!  split_digits(+Number:int, +Numbers:list) is semidet.
 %!  split_digits(+Number:int, -Numbers:list) is det.
 %!  split_digits(-Number:int, -Numbers:list) is failure.
 %!  split_digits(-Number:int, +Numbers:list) is det.
@@ -22,3 +22,36 @@ split_digits(Number, Numbers) :-
     maplist(atom_number, Numbers_, Numbers),
     number_chars(Number, Numbers_),
     !.
+
+%!  split_decimal(+Number:int, +First:int, +Rest:int) is semidet.
+%!  split_decimal(+Number:int, +First:int, -Rest:int) is semidet.
+%!  split_decimal(+Number:int, -First:int, -Rest:int) is det.
+%!  split_decimal(+Number:int, -First:int, +Rest:int) is semidet.
+%!  split_decimal(-Number:int, +First:int, +Rest:int) is semidet.
+%!  split_decimal(-Number:int, +First:int, -Rest:int) is ERROR.
+%!  split_decimal(-Number:int, -First:int, -Rest:int) is ERROR.
+%!  split_decimal(-Number:int, -First:int, +Rest:int) is ERROR.
+%
+%   Split the number into two parts.
+%
+%   Example:
+%       ?- split_decimal(1234, 1000, 234).
+%       true.
+%       ?- split_decimal(31, 30, 1).
+%       true.
+%       ?- split_decimal(600, 600, 0).
+%       true.
+%
+split_decimal(Number, First, Rest) :-
+    nonvar(Number), % deals with + X X
+    split_decimal_(Number, First, Rest),
+    !.
+split_decimal(Number, First, Rest) :-
+    var(Number),    % deals with - X X
+    plus(First, Rest, Number),
+    split_decimal_(Number, First, Rest).
+split_decimal_(Number, First, Rest) :-
+    split_digits(Number, [First_|Rest_]),
+    length(Rest_, L),
+    First is First_ * 10^L,
+    split_digits(Rest, Rest_).
