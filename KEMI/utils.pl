36e8fcc4783665c3122a83d45f54aedf975a9801
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-03-29 10:22:19 +0700

    Rename `split_decimal()` to `split_digits()`

--- KEMI/utils.pl
+++ KEMI/utils.pl
@@ -1,23 +1,23 @@
-:- module(utils,[split_decimal/2]).
+:- module(utils,[split_digits/2]).
 
 
-%!  split_decimal(+Number:int, +Numbers:list) is det.
-%!  split_decimal(+Number:int, -Numbers:list) is det.
-%!  split_decimal(-Number:int, -Numbers:list) is failure.
-%!  split_decimal(-Number:int, +Numbers:list) is det.
+%!  split_digits(+Number:int, +Numbers:list) is det.
+%!  split_digits(+Number:int, -Numbers:list) is det.
+%!  split_digits(-Number:int, -Numbers:list) is failure.
+%!  split_digits(-Number:int, +Numbers:list) is det.
 %
 %   Return the number splitted into digits.
 %
 %   Example:
-%       ?- split_decimal(1234, [1, 2, 3, 4]).
+%       ?- split_digits(1234, [1, 2, 3, 4]).
 %       true.
 %
-split_decimal(Number, Numbers) :-
+split_digits(Number, Numbers) :-
     nonvar(Number),
     number_chars(Number, Numbers_),
     maplist(atom_number, Numbers_, Numbers),
     !.
-split_decimal(Number, Numbers) :-
+split_digits(Number, Numbers) :-
     nonvar(Numbers),
     maplist(atom_number, Numbers_, Numbers),
     number_chars(Number, Numbers_),
