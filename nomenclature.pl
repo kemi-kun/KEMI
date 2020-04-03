c24abf0bc1d72bca38fd1050549e3b8e6002bd2d
Ding <ding533@hotmail.com>
2020-04-03 21:47:32 +0700

    Add iupac_name doc

--- nomenclature.pl
+++ nomenclature.pl
@@ -2,6 +2,14 @@
 :- use_module(organic,[organic_name/2]).
 :- use_module(inorganic,[inorganic_name/2]).
 
+%!  iupac_name(+Formula: string, +Name: string) is semidet.
+%!  iupac_name(+Formula: string, -Name: string) is semidet.
+%!  iupac_name(-Formula: string, +Name: string) is semidet.
+%!  iupac_name(-Formula: string, -Name: string) is ERROR.
+%
+%   @arg Formula – the chemical formula of an element or a compound
+%        Name – the IUPAC name of an element or a compound
+%
 iupac_name(Formula, Name) :-
     organic_name(Formula, Name);
     inorganic_name(Formula, Name).
