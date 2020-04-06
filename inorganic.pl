da29f60a1086ca9f48a2afcb124520400e597c15
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-04-06 23:17:38 +0700

    Fix name changes

--- inorganic.pl
+++ inorganic.pl
@@ -1,6 +1,6 @@
 :- module(inorganic,[additive_name/2,substitutive_name/2,compositional_name/2,inorganic_name/2]).
 
-:- use_module(icompositional,[boron_hydride_stoichiometric/2,general_stoichiometric/2,addition_compound_cn/2,ion_cn/2,binary_compound_cn/2,homonuclear_cn/2]).
+:- use_module(icompositional,[boron_hydride_stoichiometric_name/2,general_stoichiometric_name/2,addition_compound_cn/2,ion_cn/2,binary_compound_cn/2,homonuclear_cn/2]).
 :- use_module(isubstitutive,[parent_hydride_anion_sn/2,parent_hydride_cation_sn/2,parent_hydride_sn/2]).
 :- use_module(iadditive,[polynulcear_entity_an/2,mononuclear_entity_an/2]).
 :- use_module(ialternative,[alternative_name/2]).
@@ -29,8 +29,8 @@ compositional_name(Formula, Name) :-
     addition_compound_cn(Formula, Name).
 
 stoichiometric(Formula, Name) :-
-    general_stoichiometric(Formula, Name);
-    boron_hydride_stoichiometric(Formula, Name).
+    general_stoichiometric_name(Formula, Name);
+    boron_hydride_stoichiometric_name(Formula, Name).
 
 substitutive_name(Formula, Name) :-
     parent_hydride_sn(Formula, Name);
