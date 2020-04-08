048546971f3511904565741fc8787611e7021c50
Nutthanich Narphromar <monaprom134@gmail.com>
2020-04-09 00:45:06 +0700

    Import isubstitutive

--- main.pl
+++ main.pl
@@ -1,6 +1,7 @@
 :- use_module(nomenclature).
 :- use_module(inorganic).
 :- use_module(icompositional).
+:- use_module(isubstitutive).
 :- use_module(elements).
 :- use_module(stoichiometry).
 :- use_module(support).
