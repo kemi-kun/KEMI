01746917fc50d16fb95afbc2f3e0926f5e431f17
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-04-08 22:03:41 +0700

    Refactor `inorganic.pl`

--- inorganic.pl
+++ inorganic.pl
@@ -22,28 +22,33 @@ inorganic_name(Formula, Name) :-
 %        Name – the compositional name of an element or a compound
 %
 compositional_name(Formula, Name) :-
-    nonvar(Name) -> (
-        homonuclear_cn(Formula, Name) -> true;
-        binary_compound_cn(Formula, Name) -> true;
-        ion_cn(Formula, Name) -> true;
-        stoichiometric_name(Formula, Name) -> true;
-        addition_compound_cn(Formula, Name)
-    ), !.
-compositional_name(Formula, Name) :-
+    nonvar(Name) ->
+        compositional_name_(Formula, Name), !;
+    compositional_name_(Formula, Name).
+
+compositional_name_(Formula, Name) :-
     homonuclear_cn(Formula, Name);
     binary_compound_cn(Formula, Name);
     ion_cn(Formula, Name);
     stoichiometric_name(Formula, Name);
     addition_compound_cn(Formula, Name).
 
+
 stoichiometric_name(Formula, Name) :-
+    nonvar(Name) ->
+        stoichiometric_name_(Formula, Name), !;
+    stoichiometric_name_(Formula, Name).
+
+stoichiometric_name_(Formula, Name) :-
     general_stoichiometric_name(Formula, Name);
     boron_hydride_stoichiometric_name(Formula, Name).
+
 
 substitutive_name(Formula, Name) :-
     parent_hydride_sn(Formula, Name);
     parent_hydride_cation_sn(Formula, Name);
     parent_hydride_anion_sn(Formula, Name).
+
 
 additive_name(Formula, Name) :-
     mononuclear_entity_an(Formula, Name);
